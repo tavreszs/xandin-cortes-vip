6be8c5ca38d0e7a0c1ade09ac67ad9c4d4a2a90c
Seu Nome <seu@email.com>
2025-05-18 07:45:35 -0300

    projeto pronto

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Xandin Cortes App no ar 🚀</h1>
+    </div>
+  )
+}
